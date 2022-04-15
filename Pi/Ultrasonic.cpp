469924c7dbc4dfdb8ad2e4af3418e51c398ce495
irving11119 <irving.deboer@gmail.com>
2022-04-15 19:35:59 +0800

    Added documentation for ultrasonic.cpp

--- Pi/Ultrasonic.cpp
+++ Pi/Ultrasonic.cpp
@@ -11,10 +11,19 @@
 
 double last_range = 0;
 
+/**
+ * @brief Sets the system for a delay of specified tim 
+ * 
+ * @param ms An int representing the number of milliseconds for the system to delay
+ */
 void delay(int ms) {
     gpioDelay(1000*ms);
 }
 
+/**
+ * @brief Triggers the ultrasonic pulse from the ultrasonic sensor
+ * 
+ */
 void ping() {
 
    gpioSetMode(TRIG, PI_OUTPUT);
@@ -27,12 +36,16 @@ void ping() {
    gpioDelay(5);
    gpioSetMode(ECHO2, PI_INPUT);
    gpioSetMode(ECHO, PI_INPUT);
-
-
-
 }
-
-void range(int gpio, int level, uint32_t tick) {
+/**
+ * @brief Function to obtain the distance reading from the left ultrasonic sensor
+ * 
+ * @param gpio A GPIO Pin whose state changes 
+ * @param level A int of range 0-2, representing: 0 = change to low (a falling edge),
+ *              1 = change to high (a rising edge), 2 = no level change (a watchdog timeout) 
+ * @param tick Integer representing the number of seconds since boot
+ */
+void rangeLeft(int gpio, int level, uint32_t tick) {
 
    static double startTick, endTick;
    double diffTick;
@@ -62,7 +75,15 @@ void range(int gpio, int level, uint32_t tick) {
       }
 }
 
-void range2(int gpio, int level, uint32_t tick) {
+/**
+ * @brief Function to obtain the distance reading from the right ultrasonic sensor
+ * 
+ * @param gpio A GPIO Pin whose state changes 
+ * @param level A int of range 0-2, representing: 0 = change to low (a falling edge),
+ *              1 = change to high (a rising edge), 2 = no level change (a watchdog timeout) 
+ * @param tick Integer representing the number of seconds since boot
+ */
+void rangeRight(int gpio, int level, uint32_t tick) {
 
    static double startTick, endTick;
    double diffTick;
@@ -93,19 +114,23 @@ void range2(int gpio, int level, uint32_t tick) {
       }
 }
 
-
-
+/**
+ * @brief Sets the system to sleep for a specified time
+ * 
+ * @param t integer representing the time in seconds the System will sleep for
+ */
 void sleep(int t) {
   gpioSleep(PI_TIME_RELATIVE, t, 0);
 }
+
 int main(){
 
-   if (gpioInitialise()<0) return 1;
+   if (gpioInitialise() < 0) return 1;
 
    gpioSetMode(ECHO, PI_INPUT);
    gpioSetMode(ECHO2, PI_INPUT);
-   gpioSetAlertFunc(ECHO2,range2);
-   gpioSetAlertFunc(ECHO, range);
+   gpioSetAlertFunc(ECHO2,rangeRight);
+   gpioSetAlertFunc(ECHO, rangeLeft);
 
    while (1) {
 
